ee40eaf13943cb3ba7d04a7076f86c4fcf6fecd6
Huzaifa bin Ahmed <65243618+malikhuzaifabinahmed@users.noreply.github.com>
2024-01-10 14:34:57 +0500

    Update linerRegresion.js

--- linerRegresion.js
+++ linerRegresion.js
@@ -8,15 +8,15 @@ class LinearRegression {
      */
 
     this.X = this.addBiasTerm(X);
-    this.y = y.map((value) => [value]); // Convert y to column vector
+    this.y = y.map((value) => [BigInt(value)]); // Convert y to column vector with BigInt
 
     // Initialize weights
-    this.weights = Array.from({ length: this.X[0].length }, () => 0);
+    this.weights = Array.from({ length: this.X[0].length }, () => BigInt(0));
   }
 
   addBiasTerm(X) {
     // Add a bias term to the input data
-    return X.map((row) => [1, ...row]);
+    return X.map((row) => [BigInt(1), ...row.map(BigInt)]);
   }
 
   predict(X) {
@@ -34,11 +34,11 @@ class LinearRegression {
     return predictions;
   }
 
-  train(learningRate = 0.01, numEpochs = 1000) {
+  train(learningRate = BigInt(100), numEpochs = 1000) {
     /**
      * Train the linear regression model using gradient descent.
      *
-     * @param {number} learningRate - The learning rate for gradient descent.
+     * @param {BigInt} learningRate - The learning rate for gradient descent.
      * @param {number} numEpochs - The number of training epochs.
      */
     for (let epoch = 0; epoch < numEpochs; epoch++) {
@@ -64,12 +64,14 @@ class LinearRegression {
 
   dotProduct(a, b) {
     // Compute dot product of two vectors
-    return a.reduce((sum, value, i) => sum + value * b[i], 0);
+    return a.reduce((sum, value, i) => sum + value * b[i], BigInt(0));
   }
 
   computeGradient(error) {
     // Compute gradient
-    const gradient = Array.from({ length: this.weights.length }, () => 0);
+    const gradient = Array.from({ length: this.weights.length }, () =>
+      BigInt(0)
+    );
 
     for (let i = 0; i < this.X.length; i++) {
       for (let j = 0; j < this.weights.length; j++) {
@@ -78,7 +80,7 @@ class LinearRegression {
     }
 
     // Normalize by the number of samples
-    return gradient.map((value) => value / this.y.length);
+    return gradient.map((value) => value / BigInt(this.y.length));
   }
 }
 
